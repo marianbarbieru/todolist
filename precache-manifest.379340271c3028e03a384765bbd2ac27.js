6c9bc27cb03245e33f37dfe2ac3b8695bb4072c9
Marian Barbieru <marianbarbieru@gmail.com>
2020-07-05 22:05:51 +0300

    Updates

---
+++ precache-manifest.379340271c3028e03a384765bbd2ac27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0f4824f23f24a4bbe67cb7c98a42a5",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "7d052e5a659bcbba19af",
+    "url": "/todolist/static/css/main.b6e2c6a7.chunk.css"
+  },
+  {
+    "revision": "b21e1ac2c9b6ecd1ec03",
+    "url": "/todolist/static/js/2.eb3a3903.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.eb3a3903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d052e5a659bcbba19af",
+    "url": "/todolist/static/js/main.94e811e5.chunk.js"
+  },
+  {
+    "revision": "5a41f0122cf00b6bc18f",
+    "url": "/todolist/static/js/runtime-main.8b55999f.js"
+  }
+]);
